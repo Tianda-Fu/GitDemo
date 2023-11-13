00d0517d283dd94bb8d8b63d5df2a479b2a436dd
stig <2713594396@qq.com>
2023-11-13 18:28:09 -0500

    this is committed from Asian timezone

---
+++ src/Test/RESTAPITest.java
@@ -0,0 +1,19 @@
+package Test;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	
+	@Test
+	public void postJira() {
+		System.out.println("postjira");
+		System.out.println("postjira1");
+		System.out.println("postjira2");
+	}
+	
+	@Test
+	public void deleteTwitter() {
+		System.out.println("deleteTwitter");
+	}
+
+}
