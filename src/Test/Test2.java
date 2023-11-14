1ccf37577fea879a07261b746dbcbfbad41a68da
stig <2713594396@qq.com>
2023-11-13 19:38:21 -0500

    NA development team

--- src/Test/Test2.java
+++ src/Test/Test2.java
@@ -5,19 +5,28 @@ import org.testng.annotations.Test;
 
 public class Test2 {
 	
+	// Forza
 	@Test(groups = {"smoke"})
 	public void Forza() {
 		System.out.println("Forza Motorsports");
 	}
 	
+	//F1
 	@Test(groups = {"smoke"})
 	public void F1() {
 		System.out.println("Formular 1");
 	}
 	
+	//GT
 	@Test(groups = {"smoke"})
 	public void GT() {
 		System.out.println("Gran Turismo");
+	}
+	
+	//ACC
+	@Test(groups = {"smoke"})
+	public void ACC() {
+		System.out.println("Assetto Corsa Competizione");
 	}
 	
 	@BeforeTest
